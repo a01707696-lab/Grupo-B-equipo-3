47fb55abc67ce6fddff465cf019edc1a07124222
a01707280 <a01707280@tec.mx>
2026-01-27 04:49:59 -0600

    #5

--- backend/.vscode/app.py
+++ backend/.vscode/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Backend Flask funcionando 🚀"
+    return "Backend arrancado correctamente!! 🚀"
 
 if __name__ == "__main__":
     app.run(debug=True)

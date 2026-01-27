c9ce3d714b672bc6b0519ccb94c2358858ec756d
a01707280 <a01707280@tec.mx>
2026-01-27 06:39:37 -0600

    #6

--- main/backend/.vscode/app.py
+++ main/backend/.vscode/app.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+# Modelo de datos Task (en memoria)
+tasks = [
+    {"id": 1, "title": "Aprender Flask", "status": "pendiente"},
+    {"id": 2, "title": "Conectar frontend con backend", "status": "hecho"}
+]
 
 @app.route("/")
 def home():
     return "Backend Flask funcionando 🚀"
 
+@app.route("/tasks")
+def get_tasks():
+    return jsonify(tasks)
+
 if __name__ == "__main__":
     app.run(debug=True)
 
-print("hello")
 
